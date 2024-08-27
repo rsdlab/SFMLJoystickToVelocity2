30da450fceeba37bb5e24416de3bd1d12256791a
KatoMisa <kato@rsdlab.jp>
2024-08-27 17:53:40 +0900

    Revert "first commit"
    This reverts commit 67ef4b4cde8568ae6e155eaed6eb920a4a2e10b6.

--- build/serializer2/CMakeFiles/TestRos2Serializer_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/TestRos2Serializer_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/TestRos2Serializer_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/serializer2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/workspace/SFMLJoystickToVelocity2/serializer2
-# Build directory: /home/ubuntu/workspace/SFMLJoystickToVelocity2/build/serializer2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
